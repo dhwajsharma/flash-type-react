3fb28d38057a733669035c75f0bc14c0680afcfe
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-02-28 15:56:22 +0530

    done with navbar

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">
-    <h2>hi</h2>
-  </div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Nav from "./components/Nav/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Nav section */}
+      <Nav />
+      {/* Landing page */}
+      {/* Challenge section */}
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
