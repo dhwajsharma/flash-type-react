602799bd37dcbf099596faae39079c3ff3528137
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-02-28 17:12:03 +0530

    Implemented typewriter effect and animation.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "./Landing.css";
 import flash from "../../assets/flash.png";
+import Typewriter from "typewriter-effect";
 
 const Landing = () => {
   return (
     <div className="landing-container">
-      <div className="landing-left">
+      <div data-aos="fade-up-right" className="landing-left">
         <h1 className="landing-header">Can you type...</h1>
         <div className="typewriter-container">
-          <p>Fast?</p>
-          <p>Correct?</p>
-          <p>Quick?</p>
+          <Typewriter
+            options={{
+              strings: ["Fast?", "Correct?", "Quick?"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
       </div>
-      <div className="landing-right">
+      <div data-aos="fade-up-left" className="landing-right">
         <img className="flash-image" src={flash} alt="hero" />
       </div>
     </div>
